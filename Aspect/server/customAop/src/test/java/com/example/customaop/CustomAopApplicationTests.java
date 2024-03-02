5140b83820c172328b1e785a247ea6c0142c029c
chosunghyun18 <krismon@naver.com>
2024-03-02 16:29:30 +0900

    feat : init project

---
+++ Aspect/server/customAop/src/test/java/com/example/customaop/CustomAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
